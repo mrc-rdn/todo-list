1a24eb3d000e307cff3f8775b63a026c01ef4908
mrc-rdn <johnmarcoardina49@gmail.com>
2025-11-26 10:09:49 +0800

    hello

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,18 @@ import express from "express"
 import pg from "pg"
 import bodyParser from "body-parser";
 import cors from "cors"
+import env from 'dotenv'
+env.config();
 
 const app = express();
 const port = 3000;
-
+  
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "todos",
-    password: "12345",
-    port: 5432
+   user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
 
 db.connect();
